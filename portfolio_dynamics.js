fc418cac5d1a37fef1d2f2eb9c292a46550c11f0
Badrr_dev <111355836+Ninjaofgames@users.noreply.github.com>
2025-11-16 22:43:20 +0100

    Add files via upload

---
+++ portfolio_dynamics.js
@@ -0,0 +1,67 @@
+function writeDescription() {
+    const element = document.getElementById("dynamicTxt");
+
+    const Txt = [
+        "Game developer",
+        "Computer engineering student",
+        "Full-Stack developer",
+        "Content creator",
+        "Game designer",
+        "IT Enthusiast",
+        "Mathematics Enthusiast"
+    ];
+
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&";
+
+    let wordIndex = 0;
+
+    function animateWord() {
+        const word = Txt[wordIndex];
+        let display = [...word].map(() => "");
+        let position = 0;
+
+        function animateLetter() {
+            if (position < word.length) {
+                let scrambleCount = 0;
+
+                let scramble = setInterval(() => {
+                    display[position] = chars[Math.floor(Math.random() * chars.length)];
+                    element.textContent = display.join("");
+
+                    scrambleCount++;
+
+                    if (scrambleCount > 5) {
+                        clearInterval(scramble);
+                        display[position] = word[position];
+                        element.textContent = display.join("");
+                        position++;
+                        setTimeout(animateLetter, 40);
+                    }
+                }, 40);
+            } else {
+                setTimeout(() => deleteWord(word), 1200);
+            }
+        }
+
+        animateLetter();
+    }
+
+    function deleteWord(word) {
+        let length = word.length;
+
+        let eraser = setInterval(() => {
+            element.textContent = element.textContent.slice(0, -1);
+            length--;
+
+            if (length < 0) {
+                clearInterval(eraser);
+                wordIndex = (wordIndex + 1) % Txt.length;
+                setTimeout(animateWord, 200);
+            }
+        }, 40);
+    }
+
+    animateWord();
+}
+
+writeDescription();
